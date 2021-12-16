3e11b3f6703dae4890e4ebe35ffb673a912c4dac
tyoohoon <57979899+tyoohoon@users.noreply.github.com>
2021-12-16 15:20:25 +0700

    fix bug: put chat in dict
    เหมือนมันอ่านข้ามเพราะโหลดไม่ทัน

--- run.py
+++ run.py
@@ -13,31 +13,34 @@ WebDriverWait(driver, 30).until(
     EC.presence_of_element_located(
         (By.CSS_SELECTOR, '.list-group.list-group-flush'))
 )
-
+i = 0
 chat_list = driver.find_elements_by_class_name('list-group-item')
 chat_text_dict = {}
-i = 0
-print(type(chat_list))
-print(len(chat_list))
+
 for chat in chat_list:  # iterate customers
-    chat.click()
-    # customer_name = driver.find_element_by_css_selector(
-    #     'h4.mb-0.text-truncate')[0].innerText
-    customer_name = i
-    chat_text_dict[customer_name] = []
+    try:
+        chat.click()
+        driver.implicitly_wait(1)
+        print('-------------------------------okay')
+    except:
+        print('not okay------------------')
+        break
     WebDriverWait(driver, 30).until(
         EC.presence_of_element_located(
             (By.CLASS_NAME, 'chatsys'))
     )
+    # customer_name = driver.find_element_by_tag_name('h4')
+    customer_name = i
+    chat_text_dict[customer_name] = []
     group_bubble_list = driver.find_elements_by_css_selector(
         '.chat-primary, .chat-secondary')
-    for group_bubble in group_bubble_list:  # iterate each customer chat
-        bubble_list = driver.find_elements_by_css_selector('.chat-item-text')
-        for bubble in bubble_list:
-            chat_text_dict[customer_name].append(
-                bubble.get_attribute('innerText'))
+    # for group_bubble in group_bubble_list:  # iterate each customer chat
+    bubble_list = driver.find_elements_by_css_selector('.chat-item-text')
+    for bubble in bubble_list:
+        print(bubble.get_attribute('innerText'))
+        chat_text_dict[customer_name].append(
+            bubble.get_attribute('innerText'))
+
     i += 1
-    if (i == 7):
-        break
 
 print(chat_text_dict)
